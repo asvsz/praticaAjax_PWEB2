bdf623449b2bfdffaf2cb8ad4e3f99bc3ba63bad
Amanda Souza <amandasldsouza@gmail.com>
2023-11-13 21:41:51 -0300

    On branch main 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-const buscarGatinhos = (e) => {
-  e.preventDefault()
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=10')
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        const cats = JSON.parse(xhr.responseText)
-        cats.forEach(cat => {
-          const img = document.createElement('img')
-          const label = document.createElement('span')
-          const section = document.createElement('section')
+const getCats = () => {
+  const tarefas = fetch(`https://api.thecatapi.com/v1/images/search?limit=10`)
 
-          section.className = 'box'
-
-          img.src = cat.url
-
-          label.innerHTML =
-            `<br> Id: ${cat.id}; <br>
-          Width: ${cat.width}px; <br>
-          Heigth: ${cat.height}px`
-
-          section.appendChild(img)
-          section.appendChild(label)
-          document.querySelector("#gatinhos").appendChild(section)
-        })
-      } else {
-        alert('Erro na requisição')
-      }
-    }
-  }
-  xhr.send()
+  tarefas 
+    .then(resposta => resposta.json())
+    .then(cats => {
+      cats.forEach(cat => {
+        const img = document.createElement('img')
+        const label = document.createElement('span')
+        const section = document.createElement('section')
+      
+        section.className = 'box'
+      
+        img.src = cat.url
+      
+        label.innerHTML =
+          `<br> Id: ${cat.id}; <br>
+        Width: ${cat.width}px; <br>
+        Heigth: ${cat.height}px`
+      
+        section.appendChild(img)
+        section.appendChild(label)
+        document.querySelector("#gatinhos").appendChild(section)
+      })
+    })
+  .catch(erro => console.log(erro))
 }
+
+
 const btnMostrar = document.querySelector("#mostrar-gatinhos")
-btnMostrar.addEventListener("click", buscarGatinhos)
+btnMostrar.addEventListener("click", getCats)
 
 
 const getMarcas = () => {
@@ -41,30 +37,26 @@ const getMarcas = () => {
   tarefas 
     .then(resposta => resposta.json())
     .then(marcas => {
-      const ul = document.createElement('ul')
       marcas.forEach(marca => {
-        const li = document.createElement('li')
         const logo = document.createElement('img')
         const label = document.createElement('span')
         const section = document.createElement('section')
 
         section.className = 'box'
+
+        logo.src = marca.image?.optimized
         label.innerHTML =
-          `<br> Id: ${marcas.name}; <br>`
+          `<br> Id: ${marca.name}; <br>`
         
-        
+        section.appendChild(logo)
         section.appendChild(label)
-        document.querySelector("#gatinhos").appendChild(section)
-        
-        logo.src = marca.image?.optimized 
-        li.appendChild(logo)
-        ul.appendChild(li)
-        console.log(marca)
+        document.querySelector("#marcas").appendChild(section)
       })
-      document.body.appendChild(ul)
     })
   .catch(erro => console.log(erro))
 }
 
-const btnMarcas = document.querySelector("#marcas")
+const btnMarcas = document.querySelector("#mostrar-marcas")
 btnMarcas.addEventListener("click", getMarcas)
+
+
