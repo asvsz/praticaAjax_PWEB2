bb99619995794f40919acf59b54231bd2423a43f
Amanda Souza <amandasldsouza@gmail.com>
2023-11-13 21:25:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const buscarGatinhos = (e) => {
+  e.preventDefault()
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', 'https://api.thecatapi.com/v1/images/search?limit=10')
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const cats = JSON.parse(xhr.responseText)
+        cats.forEach(cat => {
+          const img = document.createElement('img')
+          const label = document.createElement('span')
+          const section = document.createElement('section')
+
+          section.className = 'box'
+
+          img.src = cat.url
+
+          label.innerHTML =
+            `<br> Id: ${cat.id}; <br>
+          Width: ${cat.width}px; <br>
+          Heigth: ${cat.height}px`
+
+          section.appendChild(img)
+          section.appendChild(label)
+          document.querySelector("#gatinhos").appendChild(section)
+        })
+      } else {
+        alert('Erro na requisição')
+      }
+    }
+  }
+  xhr.send()
+}
+const btnMostrar = document.querySelector("#mostrar-gatinhos")
+btnMostrar.addEventListener("click", buscarGatinhos)
+
+
+const getMarcas = () => {
+  const tarefas = fetch(`https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/data.json`)
+
+  tarefas 
+    .then(resposta => resposta.json())
+    .then(marcas => {
+      const ul = document.createElement('ul')
+      marcas.forEach(marca => {
+        const li = document.createElement('li')
+        const logo = document.createElement('img')
+        const label = document.createElement('span')
+        const section = document.createElement('section')
+
+        section.className = 'box'
+        label.innerHTML =
+          `<br> Id: ${marcas.name}; <br>`
+        
+        
+        section.appendChild(label)
+        document.querySelector("#gatinhos").appendChild(section)
+        
+        logo.src = marca.image?.optimized 
+        li.appendChild(logo)
+        ul.appendChild(li)
+        console.log(marca)
+      })
+      document.body.appendChild(ul)
+    })
+  .catch(erro => console.log(erro))
+}
+
+const btnMarcas = document.querySelector("#marcas")
+btnMarcas.addEventListener("click", getMarcas)
